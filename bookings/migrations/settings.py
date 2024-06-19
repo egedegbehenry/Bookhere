f2c1df7b1d35a8403690a2662f8b583daf11b88f
Henrr Egedegbe <egedegbehenry@gmail.com>
2024-06-19 01:26:24 +0000

    update changes

--- bookings/migrations/settings.py
+++ bookings/migrations/settings.py
@@ -1,4 +1,9 @@
 INSTALLED_APPS = [
     # ...
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     'events',
 ]
